ff2142877111a392a65bfa6bb7ec1b4c6475ba85
kanatev <aleksey.kanatev@mail.ru>
2018-11-19 19:06:07 +0500

    Добавлен второй View controller

---
+++ newApp/ViewController2.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController2.swift
+//  newApp
+//
+//  Created by AlexMacPro on 19/11/2018.
+//  Copyright © 2018 AlexMacPro. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class ViewController2: UIViewController {
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
